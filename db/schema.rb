a8a85ed79586984cf320a965b2bba35fcb86f53b
Joe Winter <joe@joeventures.com>
2016-02-02 17:08:20 -0500

    Add the plans table to the model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160201043904) do
+ActiveRecord::Schema.define(version: 20160202220540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "plans", force: :cascade do |t|
+    t.text     "title"
+    t.integer  "total"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
