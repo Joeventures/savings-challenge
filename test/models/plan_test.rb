28494b60e226a99f3ded13087548b02f0df74180
Joe Winter <joe@joeventures.com>
2016-02-09 19:01:22 -0500

    Build a few tests on the plan model

--- test/models/plan_test.rb
+++ test/models/plan_test.rb
@@ -4,4 +4,11 @@ class PlanTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "payments should add up to plan total" do
+    plan = plans(:one)
+    plan.build_payments
+    assert plan.payments_correct?
+    assert plan.sum_payments == plan.total
+  end
 end
