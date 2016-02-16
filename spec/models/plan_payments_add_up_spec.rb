571cd9552ba5771fc0fc97b03f5ef191e0d4dea9
Joe Winter <joe@joeventures.com>
2016-02-16 09:09:34 -0500

    Strengthened model test: payments should add up

--- spec/models/plan_payments_add_up_spec.rb
+++ spec/models/plan_payments_add_up_spec.rb
@@ -6,5 +6,9 @@ RSpec.describe Payment, :type => :model do
     plan.build_payments
     expect(plan.sum_payments).to eq(plan.total)
     expect(plan.payments_correct?).to be_truthy
+    expect(plan.payments.find_by(inc: 1).amount).to eq(1)
+    expect(plan.payments.find_by(inc: 5).amount).to eq(16)
+    expect(plan.payments.find_by(inc: 25).amount).to eq(91)
+    expect(plan.payments.find_by(inc: 52).amount).to eq(191)
   end
 end
